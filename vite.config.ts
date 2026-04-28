92a7edaaa71c7bce9b7278183a0eacc9d0b505e0
MaxEnocMN <max_enoc927@yahoo.com.mx>
2026-04-28 04:44:09 -0600

    Actualización en App.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: "/FocusStudy_ScrollCapture/",
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
